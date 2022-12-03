32b04dca9d46b78fb1f4278fc872cde7c83b3cdf
phanquanghieu <pqhieu84@gmail.com>
2022-12-04 02:35:29 +0700

    [BE] remove key

--- dev_server/configs/upload.js
+++ dev_server/configs/upload.js
@@ -1,7 +1,7 @@
 module.exports = {
-  accessKeyId: process.env.ACCESS_KEY_ID || 'AKIA4YK2YRBWBJKMJMXY',
-  secretAccessKey: process.env.SECRET_ACCESS_KEY || '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',
-  region: process.env.REGION || 'ap-northeast-1',
-  bucket: process.env.BUCKET || 's3-hentity-test',
-  fileHost: process.env.FILE_HOST || 'https://s3-hentity-test.s3.ap-northeast-1.amazonaws.com',
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.REGION,
+  bucket: process.env.BUCKET,
+  fileHost: process.env.FILE_HOST,
 }
